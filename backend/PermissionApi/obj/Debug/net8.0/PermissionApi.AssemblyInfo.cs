a9a4c4c4e3fc5f2876cd8c4640fd1a02ab73706a
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-07-01 15:27:46 +0930

    working

--- backend/PermissionApi/obj/Debug/net8.0/PermissionApi.AssemblyInfo.cs
+++ backend/PermissionApi/obj/Debug/net8.0/PermissionApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermissionApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3dad425c0d542ca9a0aca1559e86fe529d9854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d1fc7e2a3050843249aebea921a993f97068e7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermissionApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermissionApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
