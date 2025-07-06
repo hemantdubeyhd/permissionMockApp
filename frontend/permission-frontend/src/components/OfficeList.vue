b17cbbc2cd29a231e807d6e7f436020b22f76698
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-07-06 17:33:16 +0930

    implementing fecthing the poffices anddipslying

--- frontend/permission-frontend/src/components/OfficeList.vue
+++ frontend/permission-frontend/src/components/OfficeList.vue
@@ -1,3 +1,38 @@
 <script setup>
 import {ref, onMount}  from 'vue';
+
+const offices = ref([]);
+const error = ref([]);
+
+const fetchOffices = async () =>
+{
+    try
+    {
+        const res = await fetch('https://localhost:7047/api/office');
+        if(!res.ok) throw new Error('Failed to fecth');
+        const data =await res.json();
+        offices.value = data;
+    }
+    catch(err)
+    {
+        error.value = 'Error fetching offcie list';
+        console.error(err);
+    }
+
+};
+onMount(() =>
+{
+    fetchOffices();
+});
 </script>
+
+<template>
+    <div>
+        <h2>Office List</h2>
+        <div v-if="error" style="color: red;">{{ error }}</div>
+
+        <ul v-else>
+            <li v-for="office in offices" :key="office.id"></li>
+        </ul>
+    </div>
+</template>
